f763f20c9151406cd19c07cff3a4721ab67533f2
Olga Akukin <olyapa@gmail.com>
2025-10-06 14:45:17 +0200

    Revert "test commit3"
    This reverts commit 11f012a43c31870acca7c9f3692bf069826b7b32.

--- build.sh
+++ build.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -e
 
-if ! command -v uv >/dev/null 2>&1; then
-  curl -LsSf https://astral.sh/uv/install.sh | sh
-  . "$HOME/.local/bin/env"
-else
-  [ -f "$HOME/.local/bin/env" ] && . "$HOME/.local/bin/env"
-fi
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source "$HOME/.local/bin/env"
 
-# 2) Установить зависимости (создаст .venv)
-uv sync
-
-# 3) Все django-команды — строго через uv run
-uv run python manage.py collectstatic --noinput
-uv run python manage.py migrate
+make install
+make collectstatic
+make migrate
