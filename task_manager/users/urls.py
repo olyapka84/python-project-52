bf51c99ccb3fb823e9ab8d214c89f51e961f8df6
Olga Akukin <olyapa@gmail.com>
2025-10-07 13:25:07 +0200

    users/urls.py - little bug fixes

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path("create/", UserCreateView.as_view(), name="create"),
     path("<int:pk>/update/", UserUpdateView.as_view(), name="update"),
     path("<int:pk>/delete/", UserDeleteView.as_view(), name="delete"),
-    path("login/", UserLoginView.as_view(template_name="users/login.html"), name="login"),
+    path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", UserLogoutView.as_view(), name="logout"),
     path("profile/edit/", UserUpdateView.as_view(), name="profile_edit"),
     path(
