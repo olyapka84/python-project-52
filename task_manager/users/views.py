3129187e24de19dafbb551f875aa54544519df43
Olga Akukin <olyapa@gmail.com>
2025-10-08 20:14:22 +0200

    fix: login messages

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -82,7 +82,7 @@ class UserLoginView(LoginView):
     next_page = reverse_lazy("home")
 
     def form_valid(self, form):
-        messages.success(self.request, "Вы успешно залогинены.")
+        messages.success(self.request, "Вы залогинены")
         return super().form_valid(form)
 
 
@@ -90,5 +90,5 @@ class UserLogoutView(LogoutView):
     next_page = reverse_lazy("home")
 
     def dispatch(self, request, *args, **kwargs):
-        messages.info(request, "Вы успешно разлогинены.")
+        messages.info(request, "Вы разлогинены")
         return super().dispatch(request, *args, **kwargs)
