f5bf5b7055491ce433fed9d2670df83cce1178e0
Olga Akukin <olyapa@gmail.com>
2025-10-08 23:24:58 +0200

    fix: register form update

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -4,24 +4,52 @@ from django.contrib.auth.models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
-    first_name = forms.CharField(label='Имя', max_length=150, required=True)
-    last_name = forms.CharField(label='Фамилия', max_length=150, required=True)
+    first_name = forms.CharField(
+        label='Имя',
+        max_length=150,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+    )
+    last_name = forms.CharField(
+        label='Фамилия',
+        max_length=150,
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+    )
 
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('username', 'first_name', 'last_name', 'password1', 'password2')
+        labels = {
+            'username': 'Имя пользователя',
+            'password1': 'Пароль',
+            'password2': 'Подтверждение пароля',
+        }
+        help_texts = {
+            'username': (
+                'Не более 150 символов. Допустимы буквы, цифры и символы: @/./+/-/_.'
+            ),
+            'password1': (
+                'Пароль должен содержать минимум 8 символов и не быть слишком простым.'
+            ),
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
 
 
 class CustomAuthenticationForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["username"].label = "Имя пользователя"
-        self.fields["password"].label = "Пароль"
-        self.fields["username"].widget.attrs.update({
-            "class": "form-control",
-            "placeholder": "Имя пользователя",
+        self.fields['username'].label = 'Имя пользователя'
+        self.fields['password'].label = 'Пароль'
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Имя пользователя',
         })
-        self.fields["password"].widget.attrs.update({
-            "class": "form-control",
-            "placeholder": "Пароль",
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Пароль',
         })
