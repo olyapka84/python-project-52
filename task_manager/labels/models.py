5f1a2d6f8dc2dc104442e7c13e1846565e1f8915
Olga Akukin <olyapa@gmail.com>
2025-10-12 14:02:37 +0200

    Add created_at display for tasks and statuses

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -3,12 +3,10 @@ from django.db import models
 
 class Label(models.Model):
     name = models.CharField("Имя", max_length=100, unique=True)
-    created_at = models.DateTimeField("Дата создания", auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ("id",)
-        verbose_name = "Label"
-        verbose_name_plural = "Labels"
+        ordering = ['id']
 
     def __str__(self):
         return self.name
