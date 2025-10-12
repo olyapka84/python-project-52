220740ba5fc303a3d6d33830f8ca9d53f364f9de
Olga Akukin <olyapa@gmail.com>
2025-10-12 15:33:33 +0200

    little models fix

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Label(models.Model):
-    name = models.CharField("Имя", max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
