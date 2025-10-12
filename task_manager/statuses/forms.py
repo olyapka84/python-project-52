2e29f0f35cb9766c07115080fc420060e068e3dc
Olga Akukin <olyapa@gmail.com>
2025-10-12 14:12:25 +0200

    Unify form labels: add labels to StatusForm

--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -6,6 +6,7 @@ class StatusForm(forms.ModelForm):
     class Meta:
         model = Status
         fields = ["name"]
+        labels = {"name": "Имя"}
         widgets = {
             "name": forms.TextInput(attrs={"class": "form-control"}),
         }
